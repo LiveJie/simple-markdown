932da3e23b467522a9b38f29644b888cd5cd1428
LiveJie <18312173568@163.com>
2020-08-24 15:39:31 +0800

    fix: 图片列表删除无效

--- extend/update-img.js
+++ extend/update-img.js
@@ -52,11 +52,13 @@
                             _this.Message.error("请上传正确格式的图片！")
                             return false
                         }
+                        let sign = new Date().getTime()
+                        item.sign = sign
                         _this.fileList.push(item)
                         var read = new FileReader();
                         read.readAsDataURL(item);
                         read.onload = function (e) {
-                            addFileList(this.result, item.name)
+                            addFileList(this.result, item.name, sign)
                         }
                     }
                 }
@@ -175,7 +177,7 @@
         }
 
         //添加图片文件
-        this.addFileList = function(url, name) {
+        this.addFileList = function(url, name, sign) {
             let fileListDom = document.getElementsByClassName("file-list-wrapper")[0]
             if(fileListDom.children.length >= 5) {
                 this.Message.error("图片最多只能选择5张!")
@@ -186,7 +188,7 @@
                     <img src="${url}" alt="">
                     <div class="name">${name}</div>
                 </div>
-                <div class="right" onclick="deleteDom(this)">
+                <div class="right" data-sign="${sign}" onclick="deleteDom(this)">
                     <svg viewBox="64 64 896 896" focusable="false" class="" data-icon="delete" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"></path></svg>
                 </div>
                 <div class="file-list-linear"></div>
@@ -201,6 +203,13 @@
         //点击删除元素
         this.deleteDom = function(e) {
             e.parentNode.remove(e)
+            _this.fileList.forEach(function(item, index) {
+                if(item.sign == e.attributes[1].value) {
+                    _this.fileList.splice(index)
+                    return;
+                }
+            })
+            console.log(_this.fileList)
         }
 
         // 模拟点击上上传图片
